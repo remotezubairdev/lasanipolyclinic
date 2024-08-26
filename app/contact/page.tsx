c3a5efe866d6a341531a91510c2112adf4f31210
Zubair Dev <164741377+remotezubairdev@users.noreply.github.com>
2024-08-26 17:26:15 +0500

    Update Contact Page
    Add a bounce back button so the user may be able to go back to home from the contact page easily

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import Contact from '../components/Contact'
 import Timings from '../components/Timings'
 import Available from '../components/Available'
+import LinkBtn from '../components/LinkBtn'
 
 const page = () => {
   return (
     <main className='mt-12'>
+      <LinkBtn name='<-- Back to Home' path='/' />
       <Available />
         <section className='flex flex-wrap items-center justify-between'>
           <Contact />
