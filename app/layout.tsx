2244c1842931d140500baa2213c0223580186171
Zubair Dev <164741377+remotezubairdev@users.noreply.github.com>
2024-11-20 09:39:46 +0500

    Update layout.tsx
    SEO keywords optimisation

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ const montserrat = Montserrat({ subsets: ["latin"] });
 export const metadata = {
   title: 'Lasani Poly Clinic - Specialized Diabetes Care & Gynecology Services',
   description: 'Lasani Poly Clinic offers specialized care in diabetes, general health, and gynecology with experienced doctors.',
-  keywords: ['diabetes specialist', 'general physician', 'gynecology', 'polyclinic', 'healthcare', 'Lahore'],
+  keywords: ['diabetes specialist', 'general physician', 'gynecology', 'polyclinic', 'healthcare', 'polyclinic in lahore'],
   openGraph: {
     title: 'Lasani Poly Clinic - Specialized Diabetes Care & Gynecology Services',
     description: 'Visit Lasani Poly Clinic for top-notch care in diabetes management, general health, and women’s health by experienced doctors.',
